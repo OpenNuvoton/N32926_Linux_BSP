1980f965c121c38db1bf659d6fbf5ac230e588e0
Chih-Chiang Chang <ccchang12@nuvoton.com>
2022-08-22 15:23:32 +0800

    Fix the issue of repeating the playback of last period data

--- linux-2.6.35.4_fa92/sound/soc/w55fa92/w55fa92_pcm.c
+++ linux-2.6.35.4_fa92/sound/soc/w55fa92/w55fa92_pcm.c
@@ -100,6 +100,20 @@ static int s_PlayChannels = 0;
 	extern int DrvSPU_SetChannelVolume(u32 u32Channel, u8 u8Volume);
 #endif                                      
 
+/*------------------------------------------*/
+/* this can solve the problem of repeating 
+   playback of last period data             */
+/*------------------------------------------*/
+#define OPT_SOLVE_LAST_PERIOD_REPEATED
+#ifdef OPT_SOLVE_LAST_PERIOD_REPEATED
+#define REPORT_TIMEOUT      1
+static int  g_middle_int=0, g_end_int = 0;
+static int  g_get_dma_addr_num = 0;
+static int  g_add_timer = 0, g_del_timer = 0;
+static struct timer_list report_addr_timer;
+#endif
+/*------------------------------------------*/
+
 static const struct snd_pcm_hardware w55fa92_pcm_hardware = {
         .info			= SNDRV_PCM_INFO_INTERLEAVED |
         SNDRV_PCM_INFO_BLOCK_TRANSFER |
@@ -223,7 +237,14 @@ static irqreturn_t w55fa92_dma_interrupt(int irq, void *dev_id)
 			snd_pcm_period_elapsed(w55fa92_audio->substream[stream]);        			
 		} 
 		if (val & P_DMA_IRQ) {
-			stream = SNDRV_PCM_STREAM_PLAYBACK;        	
+			stream = SNDRV_PCM_STREAM_PLAYBACK;
+        #ifdef OPT_SOLVE_LAST_PERIOD_REPEATED
+            if ((AUDIO_READ(REG_I2S_ACTL_PSR)&P_DMA_RIA_SN)==0){
+                g_end_int = 1;
+            }else{
+                g_middle_int = 1;
+            }
+        #endif
 			AUDIO_WRITE(REG_I2S_ACTL_PSR, AUDIO_READ(REG_I2S_ACTL_PSR));
 			AUDIO_WRITE(REG_I2S_ACTL_CON, val | P_DMA_IRQ);
 			snd_pcm_period_elapsed(w55fa92_audio->substream[stream]);        			
@@ -244,7 +265,6 @@ static irqreturn_t w55fa92_dma_interrupt(int irq, void *dev_id)
 		u32Channel = 1;
 	
 		for (ii=0; ii<2; ii++)
-//		for (ii=1; ii<2; ii++)
 		{
 			if (!(AUDIO_READ(REG_SPU_CH_EN) & u32Channel))
 			{
@@ -268,6 +288,9 @@ static irqreturn_t w55fa92_dma_interrupt(int irq, void *dev_id)
 				{			
 					AUDIO_WRITE(REG_SPU_CH_EVENT, (AUDIO_READ(REG_SPU_CH_EVENT) & ~0x3F00) | END_FG);						
 					DBG("END !! \n");	        					
+                #ifdef OPT_SOLVE_LAST_PERIOD_REPEATED
+                    g_end_int = 1;
+                #endif
 				}				
 	
 				/* clear int */	
@@ -275,6 +298,9 @@ static irqreturn_t w55fa92_dma_interrupt(int irq, void *dev_id)
 				{
 					AUDIO_WRITE(REG_SPU_CH_EVENT, (AUDIO_READ(REG_SPU_CH_EVENT) & ~0x3F00) | TH_FG);														
 					DBG("MIDDLE !! \n");	        										
+                #ifdef OPT_SOLVE_LAST_PERIOD_REPEATED
+                    g_middle_int = 1;
+                #endif
 				}											
 
 				AUDIO_WRITE(REG_SPU_CH_CTRL, AUDIO_READ(REG_SPU_CH_CTRL) & ~DRVSPU_UPDATE_ALL_PARTIALS);		
@@ -410,13 +436,90 @@ static int w55fa92_dma_prepare(struct snd_pcm_substream *substream)
         return 0;
 }
 
+#ifdef OPT_SOLVE_LAST_PERIOD_REPEATED
+static void report_addr(unsigned long data)
+{
+    struct snd_pcm_substream *substream;
+    struct snd_pcm_runtime *runtime;
+    struct w55fa92_audio *w55fa92_audio;
+    int stream;
+	
+    stream = SNDRV_PCM_STREAM_PLAYBACK; 
+    substream = (struct snd_pcm_substream *) data;
+    runtime = (struct snd_pcm_runtime *) substream->runtime;
+    w55fa92_audio = runtime->private_data;
+	spin_lock(&w55fa92_audio->lock);
+
+#ifdef CONFIG_SND_SOC_W55FA92_SPU 			
+    AUDIO_WRITE(REG_AIC_MDCR,(1<<IRQ_SPU));
+    snd_pcm_period_elapsed(w55fa92_audio->substream[stream]);        
+    AUDIO_WRITE(REG_AIC_MECR,(1<<IRQ_SPU));						
+#else
+    AUDIO_WRITE(REG_AIC_MDCR,(1<<IRQ_I2S));
+    snd_pcm_period_elapsed(w55fa92_audio->substream[stream]);        
+    AUDIO_WRITE(REG_AIC_MECR,(1<<IRQ_I2S));						
+#endif
+
+    if (!g_del_timer)
+    {
+        mod_timer(&report_addr_timer, jiffies + REPORT_TIMEOUT);
+    } else
+    {
+        g_del_timer = 0;
+    }
+    spin_unlock(&w55fa92_audio->lock);
+}
+
+static void del_report_addr_timer(void)
+{
+    del_timer(&report_addr_timer);
+}
+
+static void add_report_addr_timer(struct snd_pcm_substream *substream)
+{
+	init_timer(&report_addr_timer);
+    report_addr_timer.data = (unsigned long) substream;
+    report_addr_timer.expires = jiffies +  REPORT_TIMEOUT;
+    report_addr_timer.function = report_addr;
+    add_timer(&report_addr_timer);
+}
+#endif                            
+
 int w55fa92_dma_getposition(struct snd_pcm_substream *substream,
                            dma_addr_t *src, dma_addr_t *dst)
 {
-//        struct snd_pcm_runtime *runtime = substream->runtime;
-//        struct w55fa92_audio *w55fa92_audio = runtime->private_data;
 
 //	ENTER();
+#ifdef OPT_SOLVE_LAST_PERIOD_REPEATED
+        if (g_middle_int||g_end_int)
+        {
+            if (g_get_dma_addr_num>=2)
+            {
+                g_get_dma_addr_num = 0;
+                g_middle_int = 0;
+                g_end_int = 0;
+                if (g_add_timer)
+                {
+                    g_add_timer = 0;
+                    g_del_timer = 1;
+                }
+            } else {
+                g_get_dma_addr_num = 0;
+                if (g_middle_int)
+                    g_middle_int = 0;
+                else
+                    g_end_int = 0;
+
+                if (!g_add_timer){
+                    g_add_timer = 1;
+                    g_del_timer = 0;
+                    add_report_addr_timer(substream);
+                }
+            }
+        } else {
+            g_get_dma_addr_num++;
+        }
+#endif
 	
 #if defined(CONFIG_SND_SOC_W55FA92_I2S) || defined(CONFIG_SND_SOC_W55FA92_I2S_MODULE)
         if (src != NULL)
@@ -455,6 +558,31 @@ int w55fa92_dma_getposition(struct snd_pcm_substream *substream,
 //	LEAVE();
         return 0;
 }
+
+#ifdef OPT_SOLVE_LAST_PERIOD_REPEATED
+static int w55fa92_dma_trigger(struct snd_pcm_substream *substream,
+                              int cmd)
+{
+        struct w55fa92_audio *w55fa92_audio = substream->runtime->private_data;
+        int ret = 0;	
+		spin_lock(&w55fa92_audio->lock);
+
+		ENTER();
+		
+        switch (cmd) {
+        case SNDRV_PCM_TRIGGER_STOP:
+        case SNDRV_PCM_TRIGGER_SUSPEND:
+            del_report_addr_timer();
+            g_del_timer = 1;
+            break;
+        }
+
+		LEAVE();       
+        spin_unlock(&w55fa92_audio->lock);
+        
+        return ret;
+}
+#endif
 
 static snd_pcm_uframes_t w55fa92_dma_pointer(struct snd_pcm_substream *substream)
 {
@@ -553,6 +681,9 @@ static struct snd_pcm_ops w55fa92_dma_ops = {
         .hw_params	= w55fa92_dma_hw_params,
         .hw_free	= w55fa92_dma_hw_free,
         .prepare	= w55fa92_dma_prepare,
+#ifdef OPT_SOLVE_LAST_PERIOD_REPEATED
+        .trigger    = w55fa92_dma_trigger,
+#endif
         .pointer	= w55fa92_dma_pointer,
         .mmap		= w55fa92_dma_mmap,
 };
